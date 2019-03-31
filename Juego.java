a64c8b5f9c723ff1fb41bc753ebbfc0b9c59ce9d
Nyasper <44937986+Nyasper@users.noreply.github.com>
2019-03-31 01:28:43 -0600

    Main

--- Juego.java
+++ Juego.java
@@ -1,47 +1,86 @@
 
 public class Juego {
 	public static void main(String[] args){
-		int nC=0;
 		
-		Carta arreglo[] = new Carta[72];
+		Carta cartas[] = declararCartas();
 		
-		/*arreglo[0]= new Carta("Azul",1);*/
-		/*Carta amarillo = new Carta("Amarillo",2);*/
+		for(int i = 0; i < cartas.length;i++)
+			System.out.print(cartas[i] + "\n");
+					
+	}
+	
+	public static Carta[] declararCartas(){
+		Carta cartas[] = new Carta[108];
+		int numeroDeCarta = 0;
 		
-		for(int i=0;i<9;i++){
-			for(int j=1;j<=2;j++){
-				arreglo[nC] = new Carta("Azul",i+1);
-				System.out.println(arreglo[nC]);
-				nC=nC+1;
+		//Genera cartas de colores del 1 al 9
+		for(int i = 0; i < 2;i++)
+			for(int numero = 1;numero <= 9; numero++){
+				cartas[numeroDeCarta] = new Carta("Azul", numero, "Normal");
+				numeroDeCarta++;
+				cartas[numeroDeCarta] = new Carta("Verde", numero, "Normal");
+				numeroDeCarta++;
+				cartas[numeroDeCarta] = new Carta("Rojo", numero, "Normal");
+				numeroDeCarta++;
+				cartas[numeroDeCarta] = new Carta("Amarillo", numero, "Normal");
+				numeroDeCarta++;
 			}
+		//Genera una carta con el nunmero 0 por cada color
+		cartas[72] = new Carta("Azul", 0, "Normal");
+		cartas[73] = new Carta("Verde", 0, "Normal");
+		cartas[74] = new Carta("Rojo", 0, "Normal");
+		cartas[75] = new Carta("Amarillo", 0, "Normal");
+		
+		//Genera 2 cartas RobaDos por cada color
+		numeroDeCarta = 76;
+		for(int i = 0; i < 2;i++){
+			cartas[numeroDeCarta] = new Carta("Azul", 10, "RobaDos");
+			numeroDeCarta++;
+			cartas[numeroDeCarta] = new Carta("Verde", 10, "RobaDos");
+			numeroDeCarta++;
+			cartas[numeroDeCarta] = new Carta("Rojo", 10, "RobaDos");
+			numeroDeCarta++;
+			cartas[numeroDeCarta] = new Carta("Amarillo", 10, "RobaDos");
+			numeroDeCarta++;
 		}
-		for(int i=0;i<9;i++){
-			for(int j=1;j<=2;j++){
-				arreglo[nC] = new Carta("Amarillo",i+1);
-				System.out.println(arreglo[nC]);
-				nC=nC+1;
-			}
+		
+		//Genera 2 cartas CambioSentido por cada color
+		for(int i = 0; i < 2;i++){
+			cartas[numeroDeCarta] = new Carta("Azul", 10, "CambioSentido");
+			numeroDeCarta++;
+			cartas[numeroDeCarta] = new Carta("Verde", 10, "CambioSentido");
+			numeroDeCarta++;
+			cartas[numeroDeCarta] = new Carta("Rojo", 10, "CambioSentido");
+			numeroDeCarta++;
+			cartas[numeroDeCarta] = new Carta("Amarillo", 10, "CambioSentido");
+			numeroDeCarta++;
 		}
-		for(int i=0;i<9;i++){
-			for(int j=1;j<=2;j++){
-				arreglo[nC] = new Carta("Verde",i+1);
-				System.out.println(arreglo[nC]);
-				nC=nC+1;
-			}
+		
+		//Genera 2 cartas PierdeTurno por cada color
+		for(int i = 0; i < 2;i++){
+			cartas[numeroDeCarta] = new Carta("Azul", 10, "PierdeTurno");
+			numeroDeCarta++;
+			cartas[numeroDeCarta] = new Carta("Verde", 10, "PierdeTurno");
+			numeroDeCarta++;
+			cartas[numeroDeCarta] = new Carta("Rojo", 10, "PierdeTurno");
+			numeroDeCarta++;
+			cartas[numeroDeCarta] = new Carta("Amarillo", 10, "PierdeTurno");
+			numeroDeCarta++;
 		}
-		for(int i=0;i<9;i++){
-			for(int j=1;j<=2;j++){
-				arreglo[nC] = new Carta("Rojo",i+1);
-				System.out.println(arreglo[nC]);
-				nC=nC+1;
-			}
+		
+		//Genera 4 cartas C-CambioColor
+		for(int i = 1; i <= 4; i++){
+			cartas[numeroDeCarta] = new Carta("Todos", 10, "C-CambioColor");
+			numeroDeCarta++;
+		}
+		
+		//Genera 4 cartas C-CambioColor4
+		for(int i = 1; i <= 4; i++){
+			cartas[numeroDeCarta] = new Carta("Todos", 10, "C-CambioColor4");
+			numeroDeCarta++;
 		}
 		
 		
-		CartaEspecial cartaE = new CartaEspecial("Negro");
-		System.out.println(cartaE);
-		
-
-		
+		return cartas;	
 	}
 }
