de906ec8ba1399db08aec0361be9874076fa35af
YaelJBS <47933710+YaelJBS@users.noreply.github.com>
2019-04-03 14:44:17 -0600

    Cartas

--- Juego.java
+++ Juego.java
@@ -10,8 +10,11 @@ public class Juego {
 		
 		//int jugadores = numeroDeJugadores();           //Se declara el numero de jugadores
 		Jugador jugador[] = mazoDeJugadores(jugadores, cartas); //Se declaran los jugadores
+		Carta cartasVolteadas[] = cartasEnJuego(cartas);
 		
+		//Carta cartasVolteadas[] = turno(cartas, cartasVolteadas, jugador);
 		//Imprime las cartas del mazo principal
+		/*
 		System.out.println("Cartas del mazo principal\n");
 		for(int i = 0; i < cartas.length;i++)
 			System.out.println(cartas[i]);
@@ -21,6 +24,106 @@ public class Juego {
 		for(int i = 0; i < 7;i++)
 			System.out.println(jugador[0].mazo[i]);
 		
+		System.out.println("\nPrimera Carta\n");
+		System.out.println(cartasVolteadas[0]);
+		System.out.println(cartas[93]);
+		*/
+		int cartaEntrada;
+		int jugadorNumero=0;
+		int nDeCartaVolteada=0;
+		int nDeCartaRevuelta;
+		//System.out.println(cartasVolteadas[0]);
+		while(ganador==false){
+			
+			System.out.println(cartasVolteadas[nDeCartaVolteada]);//Imprime la carta en juego
+			
+			nDeCartaVolteada++;
+			
+			System.out.println("\nCartas del jugador " + jugadorNumero +"\n");
+			
+			for(int i = 0; jugador[jugadorNumero].mazo[i] != null; i++){//Imprime las cartas del jugador en turno
+				System.out.println(jugador[jugadorNumero].mazo[i]);
+			}
+			
+			System.out.println("Ingrese una carta ");//Pide una carta al jugador
+			cartaEntrada=entrada.nextInt();
+			
+			//Si la carta no es la correcta, le vuelve a pedir otra al jugador
+			while(jugador[jugadorNumero].mazo[cartaEntrada].color != cartasVolteadas[0].color 
+					&& jugador[jugadorNumero].mazo[cartaEntrada].numero != cartasVolteadas[0].numero){
+				System.out.println("No puedes poner esa carta, pon otra");
+				cartaEntrada=entrada.nextInt();
+			}
+			
+			//Agrega la carta que eligio el jugador, a el mazo cartasVolteadas		
+			cartasVolteadas[nDeCartaVolteada]=jugador[jugadorNumero].mazo[cartaEntrada];
+			//Elimina la carta que eligio el jugador, de su mano
+			jugador[jugadorNumero].mazo[cartaEntrada] = null;
+			
+			//Recorre de posicion las cartas de la mano del jugador
+			for(int i = cartaEntrada; true; i++){
+				jugador[jugadorNumero].mazo[i] = jugador[jugadorNumero].mazo[i+1];
+				if(jugador[jugadorNumero].mazo[i]==null)
+					break;
+			}
+			
+			//Efecto carta RobaDos
+			if(cartasVolteadas[nDeCartaVolteada].efecto=="RobaDos"){
+				
+				if(jugadorNumero==0){
+					
+					for(int h = 0; h<2; h++){
+						
+						for(int i = 0; jugador[1].mazo[i] != null; i++ ){
+							
+							if(jugador[1].mazo[i+1] == null){
+								
+									for(int k = 107; cartas[k] == null || k>0; k--){
+									
+										if(cartas[k] != null){
+											jugador[1].mazo[i+1] = cartas[k];
+											cartas[k] = null;
+											break;
+										}
+									}
+									break;
+							}		
+						}
+					}
+				}
+				if(jugadorNumero==0){
+					
+					for(int h = 0; h<2; h++){
+						
+						for(int i = 0; jugador[1].mazo[i] != null; i++ ){
+							
+							if(jugador[1].mazo[i+1] == null){
+								
+									for(int k = 107; cartas[k] == null || k>0; k--){
+									
+										if(cartas[k] != null){
+											jugador[1].mazo[i+1] = cartas[k];
+											cartas[k] = null;
+											break;
+										}
+									}
+									break;
+							}		
+						}
+					}
+				}
+					
+				System.out.println(jugador[1].mazo[8]+"Hola");
+			}
+			
+			//Cambia el turno
+			if(jugadorNumero==0){
+				jugadorNumero=1;
+			}else{
+				jugadorNumero=0;
+			}
+					
+		}
 	}
 	
 	//Funcion que modela todas las cartas
@@ -144,6 +247,7 @@ public class Juego {
 		return cartasRevueltas;
 	}
 	
+	
 	//Funcion para asignar las primeras 7 cartas a cada jugador
 	public static Jugador[] mazoDeJugadores(int NumeroJugadores, Carta MazoCartas[]){ //Se pasan las cartas y numero de jugador declararado en el main
 		Carta cartas[] = MazoCartas;   //Se asisgnan las cartas del main a cartas
@@ -161,17 +265,21 @@ public class Juego {
 		//Asignacion de cartas del arreglo cartas al arreglo mazo, dentro de cada arreglo jugador
 		for(int i = 0; i < 7; i++){
 			jugador[0].mazo[cont2] = cartas[cont];//Jugador numero 1
+			cartas[cont] = null;
 			cont--;
 
 			jugador[1].mazo[cont2] = cartas[cont];//jugador numero 2
+			cartas[cont] = null;
 			cont--;
 
 			if(jugadores>2){//Asignacion de cartas al jugador 3(solo si son 3 o 4 jugadores)
 				jugador[2].mazo[cont2] = cartas[cont];//jugador numero 3
+				cartas[cont] = null;
 				cont--;
 
 				if(jugadores>3){//Asignacion de cartas al jugador 4(solo si son 4 jugadores)
 					jugador[3].mazo[cont2] = cartas[cont];//Jugador numero 4
+					cartas[cont] = null;
 					cont--;
 				}
 			}
@@ -180,4 +288,24 @@ public class Juego {
 
 		return jugador;
 	}
+	
+	//Funcion para poner la primera carta del arreglo cartas barajadas, al arreglo de cartas volteadas para empezar a jugar
+	public static Carta[] cartasEnJuego(Carta mazoCartas[]) {
+		Carta cartas[] = mazoCartas;//Arreglo de cartas barajadas
+		
+		Carta cartasVolteadas[] = new Carta[108];//Arreglo de cartas volteadas
+		
+		//Toma la ultima carta del arreglo de cartas barajadas para comenzar a jugar
+		for(int i = 107; cartas[i] == null || i>=0; i--){
+			
+			if(cartas[i] != null){
+				cartasVolteadas[0] = cartas[i];
+				cartas[i] = null;
+				break;
+			}
+		}
+		
+		return cartasVolteadas;
+	}
+	
 }
